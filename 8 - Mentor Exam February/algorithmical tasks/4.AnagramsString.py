6ad62a7e20ceef68329ec4c2c96d5ccb675a938e
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-02-16 12:26:32 +0400

    commit

--- 8 - Mentor Exam February/algorithmical tasks/4.AnagramsString.py
+++ 8 - Mentor Exam February/algorithmical tasks/4.AnagramsString.py
@@ -3,3 +3,19 @@
 # A string s (length ≤ 10⁵).
 # A string p (length ≤ 10⁴).
 
+def anagram(s, p):
+    sorted = sorted(p)  
+    result = []
+    size = len(p)
+
+    for i in range(len(s) - size + 1):  
+        current = s[i : i + size]  
+
+        if sorted(current) == sorted:  
+            result.append(i)  
+
+    return result
+
+
+print(anagram("anam", "na"))  
+print(anagram("sandro", "ndr"))         
