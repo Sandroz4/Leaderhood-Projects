8a72cc4df7eb512bad483040a138c8274605c641
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-02-16 10:39:54 +0400

    commit

---
+++ 8 - Mentor Exam February/algorithmical tasks/3.RotateArray.py
@@ -0,0 +1,20 @@
+# Given a list arr, rotate it to the right by k steps.
+# Input:
+# A list of integers arr.
+# An integer k.
+# Output: Return the rotated array.
+
+def rotate_array(arr, k):
+    size = len(arr)
+    k %= size
+    # ვაბრუნებთ
+    arr.reverse()
+    # ჯერ ვაბრუნებთ k ელემენტებს
+    arr[:k] = reversed(arr[:k])
+    # შემდეგ ვაბრუნებთ დანარჩენ ელემენტებს
+    arr[k:] = reversed(arr[k:])
+    return arr
+
+print(rotate_array([2,3,4,5,6,7,8], 5))
+print(rotate_array([2,3,4,5,6,7,8], 2))
+print(rotate_array([2,3,4,5,6,7,8], 1))
